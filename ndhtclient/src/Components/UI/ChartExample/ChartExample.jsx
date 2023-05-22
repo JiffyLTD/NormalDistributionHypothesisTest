a6e13cbc46cc1136efeb008fdf29f26559c0c645
Jiffy <120106650+JiffyLTD@users.noreply.github.com>
2023-05-22 22:04:24 +0300

    some refactor

--- ndhtclient/src/Components/UI/ChartExample/ChartExample.jsx
+++ ndhtclient/src/Components/UI/ChartExample/ChartExample.jsx
@@ -1,5 +1,4 @@
-import React, { MouseEvent, useRef } from 'react';
-import  { InteractionItem } from 'chart.js';
+import React from "react";
 import {
   Chart as ChartJS,
   LinearScale,
@@ -9,49 +8,39 @@ import {
   LineElement,
   Legend,
   Tooltip,
-} from 'chart.js';
-import {
-  Chart,
-  getDatasetAtEvent,
-  getElementAtEvent,
-  getElementsAtEvent,
-} from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';
+} from "chart.js";
+import { Chart } from "react-chartjs-2";
 
 const ChartExample = () => {
-    ChartJS.register(
-        LinearScale,
-        CategoryScale,
-        BarElement,
-        PointElement,
-        LineElement,
-        Legend,
-        Tooltip
-      );
-      const labels = ['Менее 975', '975-1000', '1000-1025', '1025-1050', 'более 1050'];
-      const data = {
-        labels,
-        datasets: [
-          {
-            type: 'line',
-            label: 'График нормального распределения',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderWidth: 2,
-            data: [7.76, 31.6, 49.7, 31.6, 7.76]
-          },
-          {
-            type: 'bar',
-            label: 'Гистограмма распределения генеральной совокупности',
-            backgroundColor: 'rgb(75, 192, 192)',
-            data: [6, 38, 44, 34, 8]
-          }
-        ],
-      };
-  return (
-    <Chart
-      data={data}
-    />
-  )
-}
+  ChartJS.register(
+    LinearScale,
+    CategoryScale,
+    BarElement,
+    PointElement,
+    LineElement,
+    Legend,
+    Tooltip
+  );
+  const data = {
+    labels: ["Менее 975", "975-1000", "1000-1025", "1025-1050", "более 1050"],
+    datasets: [
+      {
+        type: "line",
+        label: "График нормального распределения",
+        backgroundColor: "indigo",
+        borderColor: "indigo",
+        borderWidth: 4,
+        data: [7.76, 31.6, 49.7, 31.6, 7.76],
+      },
+      {
+        type: "bar",
+        label: "Гистограмма распределения генеральной совокупности",
+        backgroundColor: "MediumVioletRed",
+        data: [6, 38, 44, 34, 8],
+      },
+    ]
+  };
+  return <Chart data={data} />;
+};
 
-export default ChartExample
+export default ChartExample;
